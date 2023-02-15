8493161489d0991e632d6a7b4173fa0a12e5847a
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2023-02-15 17:28:44 -0300

    started implementing dynamic content on index.html

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render(request, "index.html")
+    data = {
+        1: {
+            "title": "Carina Nebulosa",
+            "subtitle": "Shot from Nasa / James Webb"
+        },
+        2: {
+            "title": "NGC 1659 Galaxy",
+            "subtitle": "Shot from Nasa / Hubble"
+        }
+    }
+    send = {"cards": data}
+    return render(request, "index.html", send)
 
 def imagem (request):
     return render(request, "imagem.html")
