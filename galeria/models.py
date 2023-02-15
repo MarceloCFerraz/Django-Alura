5f5df4e08796f59f281c0e15df447ec00210f1a4
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2023-02-15 19:20:52 -0300

    Implemented database data pull in index.html when loading cards

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Photography(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    subtitle = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    photo_reference = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f"Photo [title={self.title}]"
--- galeria/views.py
+++ galeria/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 
+from galeria.models import Photography
+
 # Create your views here.
 def index (request):
-    data = {
-        1: {
-            "title": "Carina Nebulosa",
-            "subtitle": "Shot from Nasa / James Webb"
-        },
-        2: {
-            "title": "NGC 1659 Galaxy",
-            "subtitle": "Shot from Nasa / Hubble"
-        }
-    }
-    send = {"cards": data}
-    return render(request, "index.html", send)
+    photos = Photography.objects.all()
+    return render(request, "index.html", {"data": photos})
 
 def imagem (request):
     return render(request, "imagem.html")
